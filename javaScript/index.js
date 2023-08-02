fde6926bbf90279231429b19c1df8d0f849ef8a0
Bruno Bornengo <fatubornengo@gmail.com>
2023-08-01 23:20:33 -0300

    segunda preentrega: arrays, funciones, objetos, dom

--- javaScript/index.js
+++ javaScript/index.js
@@ -9,12 +9,12 @@ while (bienvenida != "Si" && bienvenida != "No") {
 const cotizarSeguro=()=>{
 //Recupero los elementos que tengo en el formulario//
     let marca = document.querySelector("#marca").value;
-    let año = document.querySelector("#año").value;
+    let anio = document.querySelector("#anio").value;
     let basico = document.querySelector("#basico");
     let completo = document.querySelector("#completo");
 
-    let resumen = document.querySelector("#resumen").value;
-    let resultado = document.querySelector("#resultado").value;
+    let resumen = document.querySelector("#resumen");
+    let resultado = document.querySelector("#resultado");
 
     let plan="";
 
@@ -24,12 +24,90 @@ const cotizarSeguro=()=>{
         plan="completo";
     }
 
-    if(marca ==='' || año === '' || plan === ''){
+    if(marca ==='' || anio === '' || plan === ''){
         mostrarError("#mensaje-error", "Seleccione todas las opciones")
         return;
     }
 
-    let cotizacion={marca, año, plan};
+    let cotizacion={marca, anio, plan};
+    document.querySelector("#msj").style.display="none"
+
+    resumen.style.backgroundColor="FFF";
+    resumen.style.display="block";
+
+    
+    resumen.innerHTML=`<div style:text-align:center></div>`;
+    
+    setTimeout(()=>{
+        resumen.innerHTML=`
+                        <h2>Resumen de cotizacion</h2>
+                        <ul>
+                            <li>Marca: ${marca}</li>
+                            <li>Año: ${anio}</li>
+                            <li>Plan: ${plan}</li>
+                        </ul>`;
+        let cotizacionFinal = cotizar(cotizacion);
+        resultado.style.display="block";
+        resultado.innerHTML=`<p class="textoCotizacion">$ ${cotizacionFinal}</p>`;
+    },2000);
+}
+
+const cotizar =(cotizacion)=>{
+    const {marca,anio, plan}=cotizacion;
+    let resultado=2000;
+
+    const diferenciaAnio=diferencia(anio);
+    resultado-=((diferenciaAnio*3)*resultado)/100;
+
+    resultado = calcularMarca(marca)*resultado;
+    const incrementoPlan = obtenerPlan(plan);
+    resultado=parseFloat(incrementoPlan*resultado).toFixed(2);
+    return resultado
+}
+
+function obtenerPlan(plan){
+    if(plan==='basico'){
+      return 1.20;
+    } else {
+      return 1.50;
+    }
+  }
+
+const calcularMarca=marca=>{
+    let incremento;
+
+    switch (marca) {
+        case 'ranger':
+            incremento=1.30;
+            break;
+        
+        case 'focus':
+            incremento=1.30;
+            break;  
+
+        case 'clio':
+            incremento=1.15;
+            break;
+
+        case 'sandero':
+            incremento=1.15;
+            break;
+        
+        case 'corsa':
+            incremento=1.05;
+            break;   
+            
+        case 'onix':
+            incremento=1.05;
+            break;    
+        default:
+            break;
+    }
+    return incremento;
+}
+
+const diferencia=(anio)=>{
+    return new Date().getFullYear()-anio;
 }
 
 const mostrarError=(elemento,mensaje)=>{
